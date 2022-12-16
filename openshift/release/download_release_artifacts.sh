601d380547ebd8afdeb1863a2f3e4bf3f55736d7
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2022-12-16 13:21:45 +0900

    [SRVKS-990] Unify release manifests in net-kourier

--- openshift/release/download_release_artifacts.sh
+++ openshift/release/download_release_artifacts.sh
@@ -1,40 +1,57 @@
 #!/usr/bin/env bash
 
-# Usage example: ./download_release_artifacts.sh v1.8.0
+# Usage example: ./download_release_artifacts.sh 1.8.0
 
 set -Eeuo pipefail
 
 SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 
-# Returns the major and minor part of the whole version, joined with a dot.
-function versions.major_minor {
-  local version=${1:?Pass a full version as arg[1]}
-  # shellcheck disable=SC2001
-  # Ref: https://regex101.com/r/Po1HA3/1
-  echo "${version}" | sed 's/^v\?\([[:digit:]]\+\)\.\([[:digit:]]\+\).*/\1.\2/'
+# TODO: automatically detects the version via branch name or something.
+VERSION=$1
+
+function resolve_resources(){
+  local dir=$1
+  local resolved_file_name=$2
+
+  echo "Writing resolved yaml to $resolved_file_name"
+
+  > "$resolved_file_name"
+
+  for yaml in `find $dir -name "*.yaml" | sort`; do
+    resolve_file "$yaml" "$resolved_file_name"
+  done
 }
 
-version=$1
+function resolve_file() {
+  local file=$1
+  local to=$2
+
+  echo "---" >> "$to"
+
+  echo $file
+
+  sed -e "s+app.kubernetes.io/version: devel+app.kubernetes.io/version: \""$VERSION"\"+" \
+      "$file" >> "$to"
+
+}
+
+readonly YAML_OUTPUT_DIR="openshift/release/artifacts/"
+readonly KOURIER_YAML=${YAML_OUTPUT_DIR}/0-kourier.yaml
+readonly CONFIG_NETWORK_YAML=${YAML_OUTPUT_DIR}/1-config-network.yaml
+
+# Clean up
+rm -rf "$YAML_OUTPUT_DIR"
+mkdir -p "$YAML_OUTPUT_DIR"
+
+readonly CONFIG_NETWORK="vendor/knative.dev/networking/config/config-network.yaml"
+
 patches_path="${SCRIPT_DIR}/../patches"
-artifacts_path="${SCRIPT_DIR}/artifacts"
-mkdir -p "${patches_path}"
-mkdir -p "${artifacts_path}"
 
-url="https://github.com/knative-sandbox/net-kourier/releases/download/knative-${version}/kourier.yaml"
-kourier_file="${artifacts_path}/0-kourier.yaml"
-wget --no-check-certificate "$url" -O "$kourier_file"
 # TODO: [SRVKS-610] These values should be replaced by operator instead of sed.
-sed -i -e 's/net-kourier-controller.knative-serving/net-kourier-controller/g' "$kourier_file"
+git apply "${patches_path}/001-service-location.patch"
 
-# Download config-network.yaml for Kourier. This is necessary as kourier uses different namespace (knative-serving-ingress).
-config_network_url="https://raw.githubusercontent.com/knative/networking/release-$(versions.major_minor "${version}")/config/config-network.yaml"
-config_network="${artifacts_path}/1-config-network.yaml"
-wget --no-check-certificate "$config_network_url" -O "$config_network"
-sed -i -e '/labels:$/a \    app.kubernetes.io\/component: kourier' "$config_network"
-sed -i -e '/labels:$/a \    networking.knative.dev\/ingress-provider: kourier' "$config_network"
+# Add label to the config-network, which is deployed in knative-serving-ingress namespace.
+sed -i -e 's/app.kubernetes.io\/component: networking/app.kubernetes.io\/component: kourier\n    networking.knative.dev\/ingress-provider: kourier/' "$CONFIG_NETWORK"
 
-# Make Kourier rollout in a more defensive way so no requests get dropped.
-# TODO: Can probably be removed in 1.21 and/or be sent upstream.
-git apply "${patches_path}/001-kourier-rollout.patch"
-git apply "${patches_path}/002-backport.patch"
-git apply "${patches_path}/003-keepalive.patch"
+resolve_resources "config/" "$KOURIER_YAML"
+resolve_resources "vendor/knative.dev/networking/config/config-network.yaml" "$CONFIG_NETWORK_YAML"
