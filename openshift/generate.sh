8bdbc4cd1513d9c31daf5e2c73668f770c439d7f
Martin Gencur <mgencur@redhat.com>
2024-09-13 13:51:01 +0200

    Use @latest tag for go install

--- openshift/generate.sh
+++ openshift/generate.sh
@@ -4,14 +4,7 @@ set -euo pipefail
 
 repo_root_dir=$(dirname "$(realpath "${BASH_SOURCE[0]}")")/..
 
-tmp_dir=$(mktemp -d)
-git clone --branch main https://github.com/openshift-knative/hack "$tmp_dir"
-
-pushd "$tmp_dir"
-go install github.com/openshift-knative/hack/cmd/generate
-popd
-
-rm -rf "$tmp_dir"
+go install github.com/openshift-knative/hack/cmd/generate@latest
 
 $(go env GOPATH)/bin/generate \
   --root-dir "${repo_root_dir}" \
